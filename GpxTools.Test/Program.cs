a9330cbec15e7419284118611f43284070cd7d75
Guillaume NICOLAS <guillaume.nicolas@viacesi.frgui>
2018-12-30 17:34:46 +0100

    Ajoutez des fichiers projet.

---
+++ GpxTools.Test/Program.cs
@@ -0,0 +1,111 @@
+﻿using GpxTools.Gpx;
+using GpxTools;
+using System;
+using System.IO;
+
+namespace GpxTools.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Gpx Analyser";
+            Console.WriteLine("Enter Gpx path :");
+            var path = Console.ReadLine();
+            var GpxAnalyser = new GpxAnalyser(path);
+            GpxAnalyser.Analyse();
+            Console.Clear();
+            Console.SetWindowPosition(0, 0);
+            Console.SetWindowSize(175, 50);
+            Console.WriteLine($"distance : {Math.Round(GpxAnalyser.TotalLenght, 2)}");
+            Console.WriteLine($"denivele + : {Math.Round(GpxAnalyser.PosHeightDif, 2)}");
+            Console.WriteLine($"denivele - : {Math.Round(GpxAnalyser.NegHeightDif, 2)}");
+            Console.WriteLine($"Alt Max : {Math.Round(GpxAnalyser.MaxElevation, 0)}");
+            Console.WriteLine($"Alt Min : {Math.Round(GpxAnalyser.MinElevation, 0)}");
+            Console.WriteLine($"CalculatedTime : {GpxAnalyser.CalculatedDurationTime}");
+            Console.WriteLine($"Time : {GpxAnalyser.RealDurationTime}");
+            for (int i = 0; i < Console.BufferWidth - 2; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            var MinHeightCon = Console.CursorTop+1;
+            var MaxHeightCon = Console.WindowHeight-2;
+            var MinWightCon = 10;
+            var MaxWightCon = Console.BufferWidth - 10;
+            var WidghtCon = MaxWightCon - MinWightCon;
+            var HeightCon = MaxHeightCon - MinHeightCon;
+
+            double? MinCoordX = null;
+            double? MaxCoordX = null;
+            double? MinCoordY = null;
+            double? MaxCoordY = null;
+            foreach (var item in GpxAnalyser.Points)
+            {
+                if (MinCoordX == null || item.Latitude < MinCoordX)
+                    MinCoordX = item.Latitude;
+                if (MaxCoordX == null || item.Latitude > MaxCoordX)
+                    MaxCoordX = item.Latitude;
+                if (MinCoordY == null || item.Longitude < MinCoordY)
+                    MinCoordY = item.Longitude;
+                if (MaxCoordY == null || item.Longitude > MaxCoordY)
+                    MaxCoordY = item.Longitude;
+            }
+            var Xnegatif = false;
+            if (MinCoordX < 0)
+            {
+                Xnegatif = true;
+            }
+            var Ynegatif = false;
+            if (MinCoordY < 0)
+            {
+                Ynegatif = true;
+            }
+            var sizeX = Math.Abs((MaxCoordX - MinCoordX).Value);
+            var sizeY = Math.Abs((MaxCoordY - MinCoordY).Value);
+            Func<GpxPoint,dynamic> getPosition = new Func<GpxPoint, dynamic>((GpxPoint item) => {
+                var posX = item.Latitude;
+                if (Xnegatif)
+                {
+                    posX += -MinCoordX ?? 0;
+                }
+                else
+                {
+                    posX -= MinCoordX ?? 0;
+                }
+                var posY = item.Longitude;
+                if (Ynegatif)
+                {
+                    posY += -MinCoordY ?? 0;
+                }
+                else
+                {
+                    posY -= MinCoordY ?? 0;
+                }
+                posX = HeightCon - posX * HeightCon / sizeX + MinHeightCon;
+                posY = posY * WidghtCon / sizeY + MinWightCon;
+                return new { X = posX, Y = posY }; ;
+            });
+            foreach (var item in GpxAnalyser.Points)
+            {
+                var pos = getPosition(item);
+                Console.CursorLeft = (int)pos.Y;
+                Console.CursorTop = (int)pos.X;
+                Console.Write(".");
+            }
+            var posStart = getPosition(GpxAnalyser.Points.StartPoint);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.CursorLeft = (int)posStart.Y;
+            Console.CursorTop = (int)posStart.X;
+            Console.Write("S");
+            var posEnd = getPosition(GpxAnalyser.Points.EndPoint);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.CursorLeft = (int)posEnd.Y;
+            Console.CursorTop = (int)posEnd.X;
+            Console.Write("F");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GpxTools/GpxAnalyser.cs
@@ -0,0 +1,260 @@
+﻿using GpxTools.Gpx;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace GpxTools
+{
+    public class GpxAnalyser
+    {
+        /// <summary>
+        /// Positive height difference
+        /// </summary>
+        public double PosHeightDif { get; internal set; }
+        /// <summary>
+        /// Negative height difference 
+        /// </summary>
+        public double NegHeightDif { get; internal set; }
+
+        /// <summary>
+        /// Total lenght
+        /// </summary>
+        public double TotalLenght
+        {
+            get
+            {
+                if (Points != null)
+                    return Points.GetLength();
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Calculated duration Time of the track with setted average speeds;
+        /// </summary>
+        public TimeSpan CalculatedDurationTime
+        {
+            get
+            {
+                var time = PosHeightDif / AverageAscSpeed + NegHeightDif / AverageDescSpeed + (TotalLenght - DescDist - AscDist) / AverageFlatSpeed;
+                try
+                {
+                    return TimeSpan.FromHours(time);
+                }
+                catch (Exception)
+                {
+                    return new TimeSpan();
+                }
+            }
+        }
+        /// <summary>
+        /// Real Duration time extracted from the track
+        /// </summary>
+        public TimeSpan? RealDurationTime
+        {
+            get
+            {
+                if (Points != null && Points.EndPoint != null && Points.StartPoint != null)
+                    return Points.EndPoint.Time - Points.StartPoint.Time;
+                return null;
+            }
+        }
+        /// <summary>
+        /// Elevation Max
+        /// </summary>
+        public double MaxElevation
+        {
+            get
+            {
+                if (Points != null)
+                    return Points.GetMaxElevation() ?? 0;
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Elevation Min
+        /// </summary>
+        public double MinElevation
+        {
+            get
+            {
+                if (Points != null)
+                    return Points.GetMinElevation() ?? 0;
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Limit of Elevation difference calculation
+        /// </summary>
+        public int LimitElevationDif { get; set; }
+        /// <summary>
+        /// speed on Flat terrain in Meter / hour
+        /// Default 5000m/h
+        /// </summary>
+        public int AverageFlatSpeed { get; set; }
+        /// <summary>
+        /// Ascentional speed in KiloMeter / Hour
+        /// Default 350m/h
+        /// </summary>
+        public int AverageAscSpeed { get; set; }
+        /// <summary>
+        /// Descending speed in Meter / Hour
+        /// Default 550m/h
+        /// </summary>
+        public int AverageDescSpeed { get; set; }
+
+        /// <summary>
+        /// List of points of the track
+        /// </summary>
+        public GpxPointCollection<GpxPoint> Points
+        {
+            get
+            {
+                if (gpxReader.Track != null)
+                    return gpxReader.Track.ToGpxPoints();
+                else
+                    return null;
+            }
+        }
+
+        private readonly GpxReader gpxReader;
+
+        private GpxAnalyser()
+        {
+            LimitElevationDif = 10;
+            AverageAscSpeed = 350;
+            AverageDescSpeed = 550;
+            AverageFlatSpeed = 5;
+        }
+        /// <summary>
+        /// Create an instance of a Gpx Analyser
+        /// </summary>
+        /// <param name="gpxReader">Gpx Reader cannot be null</param>
+        public GpxAnalyser(GpxReader gpxReader) : this()
+        {
+            this.gpxReader = gpxReader ?? throw new ArgumentNullException(nameof(gpxReader));
+        }
+        /// <summary>
+        /// Create an instance of a Gpx Analyser
+        /// </summary>
+        /// <param name="Url">Url of gpx File</param>
+        public GpxAnalyser(string url) : this()
+        {
+            Stream stream = null;
+            if (IsLocalPath(url))
+            {
+                stream = new FileStream(url, FileMode.Open);
+            }
+            else
+            {
+                using (var client = new HttpClient())
+                {
+                    var get = client.GetStreamAsync(url);
+                    get.Wait();
+                    stream = get.Result;
+                }
+            }
+            gpxReader = new GpxReader(stream);
+        }
+        private static bool IsLocalPath(string p)
+        {
+            try
+            {
+                return new Uri(p).IsFile;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// calculate metrics from Gpx File
+        /// </summary>
+        public void Analyse()
+        {
+            while (gpxReader.Read())
+            {
+
+            }
+            foreach (var point in Points)
+            {
+                PosHeightDif += GetPositiveHeightDif(point);
+                NegHeightDif += GetNegativeHeightDif(point);
+            }
+        }
+        private GpxPoint lastLimitPointElePos;
+        private double AscDist;
+        private double GetPositiveHeightDif(GpxPoint point)
+        {
+            var heightDif = 0d;
+            if (lastLimitPointElePos != null)
+            {
+                heightDif = point.GetElevationDifFrom(lastLimitPointElePos) ?? 0;
+            }
+            else
+            {
+                lastLimitPointElePos = point;
+            }
+            if (heightDif > 0)
+            {
+                if (heightDif < LimitElevationDif)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (lastLimitPointElePos != null)
+                        AscDist += point.GetDistanceFrom(lastLimitPointElePos);
+                    lastLimitPointElePos = point;
+                    return heightDif;
+                }
+            }
+            else
+            {
+                if (-heightDif > LimitElevationDif)
+                {
+                    lastLimitPointElePos = null;
+                }
+            }
+            return 0;
+        }
+
+        private GpxPoint lastLimitPointEleNeg;
+        private double DescDist;
+        private double GetNegativeHeightDif(GpxPoint point)
+        {
+            var heightDif = 0d;
+            if (lastLimitPointEleNeg != null)
+            {
+                heightDif = -(point.GetElevationDifFrom(lastLimitPointEleNeg) ?? 0);
+            }
+            else
+            {
+                lastLimitPointEleNeg = point;
+            }
+            if (heightDif > 0)
+            {
+                if (heightDif < LimitElevationDif)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (lastLimitPointEleNeg != null)
+                        DescDist += point.GetDistanceFrom(lastLimitPointEleNeg);
+                    lastLimitPointEleNeg = point;
+                    return heightDif;
+                }
+            }
+            else
+            {
+                if (-heightDif > LimitElevationDif)
+                {
+                    lastLimitPointEleNeg = null;
+                }
+            }
+            return 0;
+        }
+
+    }
+}
